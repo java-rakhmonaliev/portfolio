d19ade5f94a08d0d0ac8cc7f8e9c2dd50dc58e7d
zaydiy <iamzaydiy@gmail.com>
2026-05-17 11:48:38 +0500

    portfolio v1 prod

--- infra/main.tf
+++ infra/main.tf
@@ -46,6 +46,13 @@ resource "aws_security_group" "portfolio" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
